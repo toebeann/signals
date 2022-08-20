301509f01e1ee12ad7824e08f2d2894df0e89895
Tobey Blaber <contact@tobey.me>
2022-08-21 00:51:16 +0100

    Simply build scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,22 @@
-import ts from 'rollup-plugin-ts';
+import typescript from 'rollup-plugin-ts';
 import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 export default {
+    input: './src/index.ts',
+    output: [{
+        sourcemap: true,
+        format: 'esm',
+        file: './dist/index.esm.js'
+    },
+    {
+        sourcemap: true,
+        format: 'cjs',
+        file: './dist/index.cjs.js'
+    }],
     plugins: [
-        ts({
+        typescript({
             browserslist: ["> 0.25%", "not dead"]
         }),
         terser({
@@ -11,6 +24,11 @@ export default {
                 comments: false
             },
             compress: false
-        })
+        }),
+        resolve({
+            modulesOnly: true,
+            browser: true
+        }),
+        nodePolyfills()
     ]
 };
