d88a44fe6cb288e1109b1921eb01030799cc2759
Tobey Blaber <contact@tobey.me>
2022-08-21 15:46:34 +0100

    Version bump for release

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from 'rollup-plugin-ts';
 import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
+import externals from '@yelo/rollup-node-external';
 
 export default {
     input: './src/index.ts',
@@ -15,6 +16,7 @@ export default {
         format: 'cjs',
         file: './dist/index.cjs'
     }],
+    external: externals(),
     plugins: [
         typescript({
             browserslist: ["> 0.25%", "not dead"]
