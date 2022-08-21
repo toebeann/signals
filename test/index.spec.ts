0ac4842df9c2bd7f442b6d5a894009577968f58c
Tobey Blaber <contact@tobey.me>
2022-08-21 19:29:39 +0100

    Migrate to v8 for code coverage

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,58 +1,54 @@
 /// <reference types='node' />
-import chai, { expect } from 'chai';
-import spies from 'chai-spies';
 import { AggregateSignal, TimeoutSignal, Signal, isSignal, isAbortSignal } from '../src/index';
-
-chai.use(spies);
 
 describe('AggregateSignal', () => {
     describe('when no signals passed', () => {
         const instance = new AggregateSignal();
         describe('signal', () => it('should be: undefined', () => {
-            expect(instance.signal).to.be.undefined
+            expect(instance.signal).toBeUndefined();
         }));
 
         describe('abortedSignal', () => it('should be: undefined', () => {
-            expect(instance.abortedSignal).to.be.undefined
+            expect(instance.abortedSignal).toBeUndefined();
         }));
     });
 
     describe('when only undefined values are passed', () => {
         const instance = new AggregateSignal(undefined, undefined, undefined);
         describe('signal', () => it('should be: undefined', () => {
-            expect(instance.signal).to.be.undefined
+            expect(instance.signal).toBeUndefined();
         }));
 
         describe('abortedSignal', () => it('should be: undefined', () => {
-            expect(instance.abortedSignal).to.be.undefined
+            expect(instance.abortedSignal).toBeUndefined();
         }));
     });
 
     describe('when one valid AbortSignal is passed', () => {
         const ac = new AbortController();
         const instance = new AggregateSignal(ac.signal);
-        const abort = chai.spy(() => { return });
+        const abort = jest.fn();
         (<unknown>(instance.signal) as Signal).addEventListener('abort', abort);
         ac.abort();
 
         describe('signal', () => {
             it('should be the valid AbortSignal', () => {
-                expect(instance.signal).to.equal(ac.signal)
-                    .and.to.not.be.undefined
+                expect(instance.signal).not.toBeUndefined();
+                expect(instance.signal).toBe(ac.signal);
             });
 
             it('should implement the EventTarget interface', () => {
-                expect(isSignal(instance.signal)).to.be.true
+                expect(isSignal(instance.signal)).toEqual(true);
             });
 
             it('should be aborted when the input AbortSignal aborts', () => {
-                expect(abort).to.have.been.called.exactly(1);
-                expect(instance.signal?.aborted).to.be.true;
+                expect(abort).toBeCalledTimes(1);
+                expect(instance.signal?.aborted).toEqual(true);
             });
         });
 
         describe('abortedSignal', () => it('should be the original AbortSignal', () => {
-            expect(instance.abortedSignal).to.deep.equal(ac.signal)
+            expect(instance.abortedSignal).toBe(ac.signal)
         }));
     });
 
@@ -60,29 +56,29 @@ describe('AggregateSignal', () => {
         const ac = new AbortController();
         const timeout = new TimeoutSignal(1000);
         const instance = new AggregateSignal(undefined, ac.signal, undefined, timeout.signal, undefined);
-        const abort = chai.spy(() => { return });
+        const abort = jest.fn();
         (<unknown>(instance.signal) as Signal).addEventListener('abort', abort);
         ac.abort();
 
         describe('signal', () => {
             it('should not equal either of the original AbortSignals', () => {
-                expect(instance.signal).to.not.equal(ac.signal)
-                    .and.to.not.equal(timeout.signal)
-                    .and.to.not.be.undefined
+                expect(instance.signal).not.toBe(ac.signal);
+                expect(instance.signal).not.toBe(timeout.signal);
+                expect(instance.signal).not.toBeUndefined();
             });
 
             it('should implement the EventTarget interface', () => {
-                expect(isSignal(instance.signal)).to.be.true
+                expect(isSignal(instance.signal)).toEqual(true);
             });
 
             it('should be aborted when either AbortSignals abort', () => {
-                expect(abort).to.have.been.called.exactly(1);
-                expect(instance.signal?.aborted).to.be.true;
+                expect(abort).toHaveBeenCalledTimes(1);
+                expect(instance.signal?.aborted).toEqual(true);
             });
         });
 
         describe('abortedSignal', () => it('should be the first aborted signal (ac.signal)', () => {
-            expect(instance.abortedSignal).to.deep.equal(ac.signal)
+            expect(instance.abortedSignal).toBe(ac.signal)
         }));
     });
 
@@ -93,28 +89,28 @@ describe('AggregateSignal', () => {
             ac.abort();
             const timeout = new TimeoutSignal(1000);
             const instance = new AggregateSignal(undefined, ac.signal, undefined, timeout.signal, undefined);
-            const abort = chai.spy(() => { return });
+            const abort = jest.fn();
             (<unknown>(instance.signal) as Signal).addEventListener('abort', abort);
 
             describe('signal', () => {
                 it('should equal the already aborted AbortSignal', () => {
-                    expect(instance.signal).to.equal(ac.signal)
-                        .and.to.not.equal(timeout.signal)
-                        .and.to.not.be.undefined
+                    expect(instance.signal).toBe(ac.signal);
+                    expect(instance.signal).not.toBe(timeout.signal);
+                    expect(instance.signal).not.toBeUndefined();
                 });
 
                 it('should implement the EventTarget interface', () => {
-                    expect(isSignal(instance.signal)).to.be.true
+                    expect(isSignal(instance.signal)).toEqual(true);
                 });
 
                 it('should immediately register as aborted', () => {
-                    expect(abort).to.have.been.called.exactly(0);
-                    expect(instance.signal?.aborted).to.be.true;
+                    expect(abort).not.toHaveBeenCalled();
+                    expect(instance.signal?.aborted).toEqual(true);
                 });
             });
 
             describe('abortedSignal', () => it('should be the first aborted signal (ac.signal)', () => {
-                expect(instance.abortedSignal).to.deep.equal(ac.signal)
+                expect(instance.abortedSignal).toBe(ac.signal)
             }));
         }
     );
@@ -123,45 +119,82 @@ describe('AggregateSignal', () => {
 describe('TimeoutSignal', () => {
     describe('when timeout: undefined', () => {
         const instance = new TimeoutSignal();
-        describe('signal', () => it('should be: undefined', () => { expect(instance.signal).to.be.undefined }));
-        describe('timeout', () => it('should be: undefined', () => { expect(instance.timeout).to.be.undefined }));
+        describe('signal', () => it('should be: undefined', () => { expect(instance.signal).toBeUndefined() }));
+        describe('timeout', () => it('should be: undefined', () => { expect(instance.timeout).toBeUndefined() }));
     });
 
     describe('when timeout: NaN', () => {
         const instance = new TimeoutSignal(NaN);
-        describe('signal', () => it('should be: undefined', () => { expect(instance.signal).to.be.undefined }));
-        describe('timeout', () => it('should be: undefined', () => { expect(instance.timeout).to.be.undefined }));
+        describe('signal', () => it('should be: undefined', () => { expect(instance.signal).toBeUndefined() }));
+        describe('timeout', () => it('should be: undefined', () => { expect(instance.timeout).toBeUndefined() }));
     });
 
     describe('when timeout: Infinity', () => {
         const instance = new TimeoutSignal(Infinity);
-        describe('signal', () => it('should be: undefined', () => { expect(instance.signal).to.be.undefined }));
-        describe('timeout', () => it('should be: undefined', () => { expect(instance.timeout).to.be.undefined }));
+        describe('signal', () => it('should be: undefined', () => { expect(instance.signal).toBeUndefined() }));
+        describe('timeout', () => it('should be: undefined', () => { expect(instance.timeout).toBeUndefined() }));
     });
 
     describe('when timeout: < 0', () => {
         const instance = new TimeoutSignal(-1);
         describe('signal', () => it(
             'should not be: undefined',
-            () => { expect(instance.signal).to.not.be.undefined }
+            () => { expect(instance.signal).not.toBeUndefined() }
         ));
         describe('timeout', () => it(
             'should not be: undefined',
-            () => { expect(instance.timeout).to.not.be.undefined }
+            () => { expect(instance.timeout).not.toBeUndefined() }
         ));
     });
 
     describe('when timeout: 1000', () => {
-        const instance = new TimeoutSignal(1000);
-        describe('signal', () => it(
-            'should not be: undefined',
-            () => { expect(instance.signal).to.not.be.undefined }
-        ));
+        const abort = jest.fn();
+        let instance: TimeoutSignal;
+
+        beforeEach(() => {
+            jest.useFakeTimers();
+            instance = new TimeoutSignal(1000);
+            (<unknown>instance.signal as Signal)?.addEventListener('abort', abort);
+        });
+
+        describe('signal', () => {
+            it(
+                'should not be: undefined',
+                () => { expect(instance.signal).not.toBeUndefined() }
+            );
+            it(
+                'should call abort after 1s',
+                () => {
+                    expect(abort).not.toHaveBeenCalled();
+                    jest.runAllTimers();
+                    expect(abort).toHaveBeenCalledTimes(1);
+                }
+            );
+        });
         describe('timeout', () => it(
             'should not be: undefined',
-            () => { expect(instance.timeout).to.not.be.undefined }
+            () => { expect(instance.timeout).not.toBeUndefined() }
         ));
+
+        describe('signal.aborted', () => {
+            it(
+                'should start: false',
+                () => { expect(instance.signal?.aborted).toEqual(false) }
+            );
+            it(
+                'should end: true',
+                () => {
+                    jest.runAllTimers();
+                    expect(instance.signal?.aborted).toEqual(true);
+                }
+            )
     });
+
+    afterEach(() => {
+        (<unknown>instance.signal as Signal)?.removeEventListener('abort');
+        jest.useRealTimers();
+    });
+});
 });
 
 describe('isSignal(object: unknown): object is Signal', () => {
@@ -169,27 +202,27 @@ describe('isSignal(object: unknown): object is Signal', () => {
 
     describe('when object: instanceof AbortSignal', () => {
         beforeEach(() => object = new AbortController().signal);
-        it('should return: true', () => { expect(isSignal(object)).to.be.true });
+        it('should return: true', () => { expect(isSignal(object)).toEqual(true) });
     });
 
     describe('when object: undefined', () => {
         beforeEach(() => object = undefined);
-        it('should return: false', () => { expect(isSignal(object)).to.be.false });
+        it('should return: false', () => { expect(isSignal(object)).toEqual(false) });
     });
 
     describe('when object: null', () => {
         beforeEach(() => object = null);
-        it('should return: false', () => { expect(isSignal(object)).to.be.false });
+        it('should return: false', () => { expect(isSignal(object)).toEqual(false) });
     });
 
     describe('when object: instanceof TypeError', () => {
         beforeEach(() => object = new TypeError());
-        it('should return: true', () => { expect(isSignal(object)).to.be.false });
+        it('should return: true', () => { expect(isSignal(object)).toEqual(false) });
     });
 
     describe('when object: { name: \'Foo\', message: \'Bar\' }', () => {
         beforeEach(() => object = { name: 'Foo', message: 'Bar' });
-        it('should return: false', () => { expect(isSignal(object)).to.be.false });
+        it('should return: false', () => { expect(isSignal(object)).toEqual(false) });
     });
 });
 
@@ -198,26 +231,26 @@ describe('isAbortSignal(object: unknown): object is AbortSignal', () => {
 
     describe('when object: instanceof AbortSignal', () => {
         beforeEach(() => object = new AbortController().signal);
-        it('should return: true', () => { expect(isAbortSignal(object)).to.be.true });
+        it('should return: true', () => { expect(isAbortSignal(object)).toEqual(true) });
     });
 
     describe('when object: undefined', () => {
         beforeEach(() => object = undefined);
-        it('should return: false', () => { expect(isAbortSignal(object)).to.be.false });
+        it('should return: false', () => { expect(isAbortSignal(object)).toEqual(false) });
     });
 
     describe('when object: null', () => {
         beforeEach(() => object = null);
-        it('should return: false', () => { expect(isAbortSignal(object)).to.be.false });
+        it('should return: false', () => { expect(isAbortSignal(object)).toEqual(false) });
     });
 
     describe('when object: instanceof TypeError', () => {
         beforeEach(() => object = new TypeError());
-        it('should return: true', () => { expect(isAbortSignal(object)).to.be.false });
+        it('should return: true', () => { expect(isAbortSignal(object)).toEqual(false) });
     });
 
     describe('when object: { name: \'Foo\', message: \'Bar\' }', () => {
         beforeEach(() => object = { name: 'Foo', message: 'Bar' });
-        it('should return: false', () => { expect(isAbortSignal(object)).to.be.false });
+        it('should return: false', () => { expect(isAbortSignal(object)).toEqual(false) });
     });
 });
