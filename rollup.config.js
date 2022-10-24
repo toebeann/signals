be99e7409cfede615af2e79defdda22c757859a4
Tobey Blaber <contact@tobey.me>
2022-10-24 14:41:42 +0100

    Update devDependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
 import typescript from 'rollup-plugin-ts';
-import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
 import externals from '@yelo/rollup-node-external';
 
 export default {
@@ -23,16 +21,9 @@ export default {
         typescript({
             browserslist: ['> 0.25%', 'not dead'],
         }),
-        terser({
-            format: {
-                comments: false,
-            },
-            compress: false,
-        }),
         resolve({
             modulesOnly: true,
             browser: true,
         }),
-        nodePolyfills(),
     ],
 };
